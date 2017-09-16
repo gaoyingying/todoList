e4d68fa49116f6a42ba363acf9e79088f620fa4f
gaoyingying <771509430@qq.com>
2017-09-16 16:20:38 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+/**
+ * Created by 高 莹 on 2017/9/16.
+ */
+$(function () {
+    $(window).resize();
+   init();
+   bind();
+});
+function init() {
+    $('.wrap').css("display",'block');
+
+}
+function bind() {
+    $('.todoList ol').on('mouseenter mouseleave','li',function (e) {
+        $(e.target).find('.del').toggleClass('active')
+    })
+    $('.wrap button').click(function () {
+        var value=$('.wrap input').val();
+        if(value!=""){
+        var li='<li>'+value+'<span class="fa fa-times del pull-right"></span></li>';
+        $('.todoList ol').append(li);
+        $('.wrap input').val("");
+        }
+
+    })
+    $('.todoList ol').on('click','li .del',function (e) {
+        $(e.target).parent().remove();
+    });
+}
+$(window).resize(function () {
+    var winWidth=$(window).width();
+    var winHeight=$(window).height();
+    document.body.clientWidth=winWidth;
+    document.body.clientHeight=winHeight;
+    $('.todo').css("height",winHeight/2);
+    $('.todo').css("marginTop",(winHeight-$('.todo').outerHeight())/2);
+
+})
